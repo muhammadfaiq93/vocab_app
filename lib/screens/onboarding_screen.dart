2c4030693aef6de0bfe95efa0dae5b67096d73b0
Muhammad Faiq <69632263+muhammadfaiq93@users.noreply.github.com>
2025-09-28 18:10:27 +0400

    onboarding_screen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -24,259 +24,46 @@ class OnboardingScreen extends StatelessWidget {
                 flex: 3,
                 child: Container(
                   width: double.infinity,
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 5),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      // Child character illustration
-                      Stack(
-                        alignment: Alignment.center,
-                        clipBehavior: Clip.none,
-                        children: [
-                          // Main child character
-                          Container(
-                            width: 180,
-                            height: 220,
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                // Body
-                                Positioned(
-                                  bottom: 0,
-                                  child: Container(
-                                    width: 120,
-                                    height: 140,
-                                    child: Column(
-                                      children: [
-                                        // Torso - Yellow shirt
-                                        Container(
-                                          width: 80,
-                                          height: 70,
-                                          decoration: BoxDecoration(
-                                            color: Color(0xFFFDD835),
-                                            borderRadius:
-                                                BorderRadius.circular(20),
-                                          ),
-                                        ),
-                                        SizedBox(height: 5),
-                                        // Legs - Blue pants
-                                        Container(
-                                          width: 70,
-                                          height: 50,
-                                          decoration: BoxDecoration(
-                                            color: Color(0xFF1976D2),
-                                            borderRadius:
-                                                BorderRadius.circular(15),
-                                          ),
-                                        ),
-                                        SizedBox(height: 5),
-                                        // Shoes
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              width: 25,
-                                              height: 15,
-                                              decoration: BoxDecoration(
-                                                color: Color(0xFF1976D2),
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                              ),
-                                            ),
-                                            SizedBox(width: 10),
-                                            Container(
-                                              width: 25,
-                                              height: 15,
-                                              decoration: BoxDecoration(
-                                                color: Color(0xFF1976D2),
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-
-                                // Head
-                                Positioned(
-                                  top: 0,
-                                  child: Container(
-                                    width: 100,
-                                    height: 100,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFFD7940C),
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Stack(
-                                      children: [
-                                        // Hair
-                                        Positioned(
-                                          top: 5,
-                                          left: 15,
-                                          right: 15,
-                                          child: Container(
-                                            height: 45,
-                                            decoration: BoxDecoration(
-                                              color: Color(0xFF5D4037),
-                                              borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(35),
-                                                topRight: Radius.circular(35),
-                                                bottomLeft: Radius.circular(20),
-                                                bottomRight:
-                                                    Radius.circular(20),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        // Eyes
-                                        Positioned(
-                                          top: 45,
-                                          left: 25,
-                                          child: Row(
-                                            children: [
-                                              // Left eye
-                                              Container(
-                                                width: 12,
-                                                height: 12,
-                                                decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  shape: BoxShape.circle,
-                                                ),
-                                                child: Center(
-                                                  child: Container(
-                                                    width: 6,
-                                                    height: 6,
-                                                    decoration: BoxDecoration(
-                                                      color: Color(0xFF5D4037),
-                                                      shape: BoxShape.circle,
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                              SizedBox(width: 25),
-                                              // Right eye
-                                              Container(
-                                                width: 12,
-                                                height: 12,
-                                                decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  shape: BoxShape.circle,
-                                                ),
-                                                child: Center(
-                                                  child: Container(
-                                                    width: 6,
-                                                    height: 6,
-                                                    decoration: BoxDecoration(
-                                                      color: Color(0xFF5D4037),
-                                                      shape: BoxShape.circle,
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        // Smile
-                                        Positioned(
-                                          top: 70,
-                                          left: 30,
-                                          child: Container(
-                                            width: 40,
-                                            height: 20,
-                                            decoration: BoxDecoration(
-                                              color: Color(0xFFE53935),
-                                              borderRadius: BorderRadius.only(
-                                                bottomLeft: Radius.circular(20),
-                                                bottomRight:
-                                                    Radius.circular(20),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-
-                                // Left arm (pointing up)
-                                Positioned(
-                                  top: 80,
-                                  left: 10,
-                                  child: Transform.rotate(
-                                    angle: -0.8,
-                                    child: Container(
-                                      width: 15,
-                                      height: 40,
+                      // Child character image from assets - increased size
+                      Container(
+                        width: 320,
+                        height: 320,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          clipBehavior: Clip.none,
+                          children: [
+                            // Main character image
+                            Container(
+                              width: 320,
+                              height: 320,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  'assets/images/child_character.png', // Replace with your image filename
+                                  fit: BoxFit.contain,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    // Fallback if image not found
+                                    return Container(
                                       decoration: BoxDecoration(
-                                        color: Color(0xFFD7940C),
-                                        borderRadius: BorderRadius.circular(10),
+                                        color: Color(0xFFE3F2FD),
+                                        borderRadius: BorderRadius.circular(15),
                                       ),
-                                    ),
-                                  ),
+                                      child: Icon(
+                                        Icons.image,
+                                        size: 80,
+                                        color: Color(0xFF6366F1),
+                                      ),
+                                    );
+                                  },
                                 ),
-
-                                // Right arm holding book
-                                Positioned(
-                                  top: 90,
-                                  right: 20,
-                                  child: Container(
-                                    width: 15,
-                                    height: 35,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFFD7940C),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                ),
-
-                                // Book
-                                Positioned(
-                                  top: 110,
-                                  right: 5,
-                                  child: Container(
-                                    width: 20,
-                                    height: 25,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF1976D2),
-                                      borderRadius: BorderRadius.circular(3),
-                                      border: Border.all(
-                                          color: Colors.white, width: 1),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          // Lightbulb idea
-                          Positioned(
-                            top: -20,
-                            right: 20,
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFDD835),
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Color(0xFFFDD835).withOpacity(0.4),
-                                    blurRadius: 15,
-                                    offset: Offset(0, 5),
-                                  ),
-                                ],
-                              ),
-                              child: Icon(
-                                Icons.lightbulb,
-                                color: Colors.white,
-                                size: 28,
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ],
                   ),
